614bbd6f2455c68b3519ef8b7472c7b01fda8d76
Mrjiong-ff <56230028+Mrjiong-ff@users.noreply.github.com>
2021-04-24 11:37:47 +0100

    pi v4.24

---
+++ runv1/main.cpp
@@ -0,0 +1,42 @@
+#include<color.h>
+#include<serial.h>
+#include<time.h>
+int main(){
+    comconfigure com;
+    colorknow know;
+    int fd,result=0;
+    int nm;
+    struct termios options;
+    fd = open(COM_NAME, O_RDWR | O_NOCTTY |O_NDELAY);
+    int q;
+    char rq[512]={"0"};
+    char buff;
+    char auff;
+    char buffer[512];
+    char len;
+    memset(buffer,0,sizeof(buffer));
+    com.set_opt(fd,115200,8,'N',1);
+    buff  = 'b';
+    auff  = 'a';
+    nm = 1;
+    while (1)
+    {
+    read(fd,rq,512);
+    rq[0]+=96;
+    rq[nm]='\0';
+//    if(rq[0]=='b'){
+    result=know.color();
+//    }
+//    if(result==1){
+//        cout<<"qnmd"<<endl;
+//        for (int i=0;i<1;i++) {
+//            write(fd,&buff,8);
+//        }
+//    }
+    char(key) = (char)waitKey(1);
+    if(key == 27){
+    break;
+    }
+     }
+    return 0;
+}
